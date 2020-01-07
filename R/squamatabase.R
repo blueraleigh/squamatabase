9729300c44ddc8737146974ba0d76ba543043067
Michael Grundler <mcgrundler@gmail.com>
2020-01-07 08:43:31 -0500

    add package doc

---
+++ R/squamatabase.R
@@ -0,0 +1,14 @@
+#' squamatabase: a database of diet samples from the world's snakes
+#'
+#' @description For a description of the database see `?diet` or `help(diet)`
+#'
+#' @section Functions:
+#' filter_records
+#' group_prey
+#' aggregate_records
+#' prey_coarse
+#' prey_detailed
+#'
+#' @docType package
+#' @name squamatabase
+NULL
